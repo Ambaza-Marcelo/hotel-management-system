a81230eedbc97551eb12030c3abad9eee625d70d
Ambaza-Marcelo <84043779+Ambaza-Marcelo@users.noreply.github.com>
2021-10-30 01:28:39 +0200

    destroy get and post report route

--- routes/web.php
+++ routes/web.php
@@ -52,10 +52,6 @@ Route::middleware(['auth', 'admin'])->group(function () {
     Route::get('/settings', 'SettingController@index')->name('settings.index');
     Route::get('admin/images','FileController@create');
     Route::post('file','FileController@store');
-    
-
-    //Route::get('admin/report','ReportController@create');
-    //Route::post('admin/report','ReportController@store');
 
     Route::resource('categories','CategoryController');
 
