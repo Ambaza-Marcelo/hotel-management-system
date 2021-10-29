475cec2d429d6d63f162842b1b31fb900cff93d8
Ambaza-Marcelo <84043779+Ambaza-Marcelo@users.noreply.github.com>
2021-10-30 01:42:50 +0200

    Hotel model

--- app/Hotel.php
+++ app/Hotel.php
@@ -5,19 +5,6 @@ namespace App;
 use App\Model;
 
 class Hotel extends Model
-{
-  /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'about','adress','image', 'language', 'code', 'theme',
-    ];
 
-  public function users()
-  {
-    return $this->hasMany('App\User');
-  }
 
 }
