abf13c491cf5a55598533fd0a7224d34d7f7e495
Ambaza-Marcelo <84043779+Ambaza-Marcelo@users.noreply.github.com>
2021-10-30 01:49:07 +0200

    checkAdmin middleware

--- app/Http/Middleware/CheckAdmin.php
+++ app/Http/Middleware/CheckAdmin.php
@@ -1,23 +1,2 @@
-<?php
 
-namespace App\Http\Middleware;
-
-use Closure;
-
-class CheckAdmin
-{
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle($request, Closure $next)
-    {
-        if (\Auth::user()->hasRole('admin') || \Auth::user()->hasRole('technician')) {
-            return $next($request);
-        }
-        return redirect('home');
-    }
 }
